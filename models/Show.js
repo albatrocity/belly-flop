9f3cf43225db73ac9d34a32498c9675c34572f21
Ross Brown <ross@cremalab.com>
2016-10-04 20:47:23 -0500

    adding URL and images to shows

--- models/Show.js
+++ models/Show.js
@@ -15,6 +15,8 @@ Show.add({
   date: { type: Types.Datetime, required: true, initial: true, format: 'Do MMM YYYY hh:mm a' },
   venue: { type: Types.Relationship, ref: 'Organization', index: true, initial: true, required: true },
   bands: { type: Types.Relationship, ref: 'Band', many: true, label: 'High Dive Band(s)', initial: true },
+  eventPage: { type: Types.Url, initial: true },
+  images: { type: Types.CloudinaryImages, autoCleanup: true },
   info: { type: Types.Textarea, initial: true, note: `cover, other bands, etc.` },
   notes: { type: Types.Textarea, initial: true, note: `how'd you set this up, etc.` }
 })
