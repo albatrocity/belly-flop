a04147b4f2c20cae2888ac8d2e05b7ee84288051
Ross Brown <ross@cremalab.com>
2016-10-12 19:10:03 -0500

    auto-populate stuff from Facebook event

---
+++ test/stub/facebookEvent.js
@@ -0,0 +1,16 @@
+module.exports = { description: 'WAVELESS\nDeranged Minneapolis psychedelic dream\n\nW/ Kansas City Bad A$$es\nCHASM\nWARM BODIES',
+  end_time: '2016-10-26T01:00:00-0500',
+  name: 'Shy Boys, Fullbloods, Warm Bodies',
+  place:
+   { name: 'The Blind Tiger',
+     location:
+      { city: 'Kansas City',
+        country: 'United States',
+        latitude: 39.0550131,
+        longitude: -94.5859645,
+        state: 'MO',
+        street: '3945 Main St',
+        zip: '64111' },
+     id: '873080142757115' },
+  start_time: '2016-10-25T21:00:00-0500',
+  id: '1804854839797620' }
